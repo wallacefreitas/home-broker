096239c4f0847253cddf620dffaa7929654cfabc
Wallace Freitas <wallacefreitas@Wallaces-MacBook-Pro.local>
2025-02-19 10:53:16 -0300

    chore: initial implement with next

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+import flowbite from "flowbite-react/tailwind";
+//@ts-expect-error - flowbite-typography has not a type
+import flowbiteTypography from "flowbite-typography";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [flowbite.plugin(), flowbiteTypography],
+} satisfies Config;
